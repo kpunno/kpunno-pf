868ac020f8e6d7340b126cbc01ccffedfc1a6877
Kristjan Punno <k.punno@gmail.com>
2025-01-29 16:46:30 -0500

    fix alignment issue, add transitions

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -7,13 +7,15 @@ const SidePanel = (toggle) => {
   const [open, setOpen] = useState(false);
 
   return open ? (
-    <div className="sidepanel fixed flex w-full max-w-[12rem] flex-col rounded-r-xl bg-white text-gray-700 shadow-xl shadow-gray-900/5">
-      <div className="flex items-center justify-between p-2 mb-1">
+    <div className="sidepanel h-full fixed flex w-full max-w-[14rem] flex-col rounded-r-xl bg-white text-gray-700 shadow-xl shadow-gray-900/5 bg-opacity-75 p-0">
+      <div className="flex items-center justify-between p-2 m-0">
+      
         {/*<img className="w-[36] h-[36]" src={"./../favicon.ico"}></img>*/}
-        <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-          &raquo; kpunno
-        </h5>
+        <strong className="block font-sans text-l antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+          kpunno
+        </strong>
         <svg
+          className="transition-transform ease-linear hover:-translate-x-1"
           onClick={()=>setOpen(false)}
           xmlns="http://www.w3.org/2000/svg"
           height="24px"
@@ -25,40 +27,42 @@ const SidePanel = (toggle) => {
         </svg>
       </div>
       <hr />
-      <nav className="flex min-w-[240px] flex-col gap-1 p-2 font-sans text-base font-normal text-blue-gray-700">
+      <nav className="flex min-w-[120px] flex-col font-sans text-base font-normal text-blue-gray-700">
         <div
           role="button"
-          className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          className="flex items-center w-full p-3 transition-all rounded-lg outline-none hover:text-[#FA0FFF] focus:text-[#FA0FFF] hover:translate-x-1"
         >
-          <Link onClick={()=>setOpen(false)} href="/">Home</Link>
+          <Link onClick={()=>setOpen(false)} href="./">Home</Link>
         </div>
         <div
           role="button"
-          className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          className="disabled flex items-center w-full p-3 transition-all rounded-lg outline-none hover:text-[#FA0FFF] focus:text-[#FA0FFF] hover:translate-x-1"
         >
           About
         </div>
         <div
           role="button"
-          className="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
+          className="flex items-center w-full p-3 transition-all rounded-lg outline-none hover:text-[#FA0FFF] focus:text-[#FA0FFF] hover:translate-x-1"
         >
           <Link onClick={()=>setOpen(false)} href="/projects">Projects</Link>
         </div>
       </nav>
     </div>
   ) : (
-    <div className="p-2 mb-1 fixed flex w-full max-w-[12rem] flex-col rounded-xl text-gray-700">
-      <svg
-          onClick = {() => setOpen(true)}
-          xmlns="http://www.w3.org/2000/svg"
-          height="24px"
-          viewBox="0 -960 960 960"
-          width="24px"
-          fill="#FA0FFF"
-        >
-          <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm280-80h280v-560H480v560Z" />
-        </svg>
-    </div>
+    <div className="flex fixed justify-between items-center p-2 mb-1 hover:text-[#FA0FFF] focus:text-[#FA0FFF]">
+  <svg
+    className="transition-transform ease-linear hover:translate-x-1"
+    onClick={() => setOpen(true)}
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#FA0FFF"
+  >
+    <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm280-80h280v-560H480v560Z" />
+  </svg>
+</div>
+
   );
 };
 
