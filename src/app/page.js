42469c1f8e492f4d8bb4ce29c2ad24187622842c
Kristjan Punno <k.punno@gmail.com>
2025-01-29 14:37:36 -0500

    add projects section routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,27 @@
+
+// Client-side routing
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center items-center">
-        <p>Test</p>
+      {/* dark:text-white */}
+      <div className="px-40 py-20">
+        <div>
+          <h1 className="text-[4rem]">kristjan punno</h1>
+        </div>
+        <br />
+        <div className="flex">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            sodales vulputate euismod. Proin condimentum massa urna, non
+            faucibus est vestibulum nec. Curabitur id finibus justo. Vivamus
+            facilisis hendrerit orci at pharetra. Vestibulum eu magna in quam
+            fringilla eleifend quis ut quam. Duis efficitur elementum magna,
+            quis dignissim elit gravida vel. Nullam cursus mauris eget tellus
+            consequat ultrices.{" "}
+          </p>
+        </div>
       </div>
     </>
   );
---
+++ src/app/projects/page.js
@@ -0,0 +1,7 @@
+// Page with demonstrations, links to projects ...
+
+export default function Projects() {
+    return (
+        <p>Projects</p>
+    )
+}
