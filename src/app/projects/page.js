6a08eb42527b5505232ff157ab57cf80fe506a75
Kristjan Punno <k.punno@gmail.com>
2025-01-29 16:46:12 -0500

    minor content adjustments

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,7 +1,28 @@
-// Page with demonstrations, links to projects ...
+
+// Client-side routing
+import Link from "next/link";
 
 export default function Projects() {
-    return (
-        <p>Projects</p>
-    )
+  return (
+    <>
+      {/* dark:text-white */}
+      <div className="px-40 py-20">
+        <div>
+          <h1 className="text-[4rem]">projects</h1>
+        </div>
+        <br />
+        <div className="flex">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+            sodales vulputate euismod. Proin condimentum massa urna, non
+            faucibus est vestibulum nec. Curabitur id finibus justo. Vivamus
+            facilisis hendrerit orci at pharetra. Vestibulum eu magna in quam
+            fringilla eleifend quis ut quam. Duis efficitur elementum magna,
+            quis dignissim elit gravida vel. Nullam cursus mauris eget tellus
+            consequat ultrices.{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
