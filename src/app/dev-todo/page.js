e90ea1808c907328adcf7f013ea4ae812994e377
Kristjan Punno <k.punno@gmail.com>
2025-01-29 18:45:24 -0500

    update todo

--- src/app/dev-todo/page.js
+++ src/app/dev-todo/page.js
@@ -3,6 +3,7 @@ export default function todo() {
     <div className="justify-center px-6 max-w-[48rem] mx-auto py-20">
       <ul className="mx-auto list-disc">
         <li>Reformat everything to match home page</li>
+        <li>Projects section wells</li>
         <li>Integrate 6502js into the website, give credit to author</li>
         <li>Permissions for AIChefBot</li>
       </ul>
