403bdd4c63e15da9bd603540bbe03a5bd84534b8
Kristjan Punno <k.punno@gmail.com>
2025-02-03 20:00:21 -0500

    update dev note

--- src/app/dev-todo/page.js
+++ src/app/dev-todo/page.js
@@ -2,13 +2,9 @@ export default function todo() {
   return (
     <div className="justify-center px-6 max-w-[48rem] mx-auto py-20">
       <ul className="mx-auto list-disc">
-        <li>Reformat everything to match home page</li>
         <li>Projects section wells</li>
         <li>Integrate 6502js into the website, give credit to author</li>
         <li>Permissions for AIChefBot</li>
-        <li>Reformat layout to include homepage styling</li>
-	<li>Fix that buttons z-index is over top layout</li>
-	<li>Fix canvas sizing issue on fullscreen (spreads out of div)</li>
       </ul>
     </div>
   );
