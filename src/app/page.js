be68bf52702d613d9991a2c4d8221aee8778889e
Kristjan Punno <k.punno@gmail.com>
2025-07-24 08:15:16 -0400

    restyle nav

--- src/app/page.js
+++ src/app/page.js
@@ -10,10 +10,12 @@ export default function Home() {
       <div className="flex items-center space-x-14">
         <h1 className="pb-2 text-[#1f1f1f]">Kristjan Punno</h1>
         {/* Icon? */}
-        {/*<img className="h-[6rem] w-[6rem]" src="./favicon.ico" alt="Kristjan's icon" />*/}
+        <img className="h-[6rem] w-[6rem]" src="./favicon.ico" alt="Kristjan's icon" />
       </div>
+
       <hr className="invisible py-2" />
       <hr className="invisible py-2" />
+      
       <div>
         <div>
           <h2 className="pb-2 text-[#1f1f1f]">Projects</h2>
@@ -36,39 +38,6 @@ export default function Home() {
               AIChefBot
             </Link>
             <br />
-          </p>
-          <br />
-        </div>
-        <div>
-          <h2 className="pb-2 text-[#1f1f1f]">Social</h2>
-          <hr className="border-t-2 border-[#BBB] w-[50%]" />
-        </div>
-        <div>
-          <p>
-            <br />
-            <a
-              className="text-[#905000] underline hover:text-[#C0A000]"
-              href="mailto:k.punno@gmail.com"
-              target="_blank"
-            >
-              k.punno@gmail.com
-            </a>
-            <br />
-            <a
-              className="text-[#905000] underline hover:text-[#C0A000]"
-              href="https://www.linkedin.com/in/kristjan-punno-68a873296/"
-              target="_blank"
-            >
-              LinkedIn
-            </a>
-            <br />
-            <a
-              className="text-[#905000] underline hover:text-[#C0A000]"
-              href="https://github.com/kpunno"
-              target="_blank"
-            >
-              GitHub
-            </a>
           </p>
           <br />
         </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,34 +1,48 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
 import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className="bg-white bg-opacity-10 backdrop-blur-sm shadow-md px-6 py-4 sticky top-0 z-50">
       <div className="flex justify-between items-center">
         <div className="md:flex space-x-4">
-          <Link
-            href="/"
-            className="text-gray-700 hover:text-[#FFAA00] transition-all"
-          >
-            Home
-          </Link>
-          <Link
-            href="/projects"
-            className="text-gray-700 hover:text-[#FFAA00] transition-all"
-          >
-            Projects
-          </Link>
-          {/*
-          <Link
-            href="/about"
-            className="text-gray-700 hover:text-[#FFAA00] transition-all"
-          >
-            About
-          </Link>
-          */}
+          {/* HOME */}
+          {pathname === "/" ? (
+            <span className="text-[#00AAFF] select-none">Home</span>
+          ) : (
+            <Link
+              href="/"
+              className="text-gray-700 hover:text-[#0000FF] transition-all"
+            >
+              Home
+            </Link>
+          )}
+          {/* PROJECTS */}
+          {pathname === "/projects" ? (
+            <span className="text-[#00AAFF] select-none">Projects</span>
+          ) : (
+            <Link
+              href="/projects"
+              className="text-gray-700 hover:text-[#0000FF] transition-all"
+            >
+              Projects
+            </Link>
+          )}
+          {/* BLOG */}
+          {pathname === "/blog" ? (
+            <span className="text-[#00AAFF] select-none">Blog</span>
+          ) : (
+            <Link
+              href="/blog"
+              className="text-gray-700 hover:text-[#0000FF] transition-all"
+            >
+              Blog
+            </Link>
+          )}
         </div>
       </div>
     </nav>
